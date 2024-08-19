eb071d023fda0c14affc4efc54152c01254ab930
César Billalta Yamasato <cesar.yamasato@usp.br>
2024-08-18 23:35:15 -0300

    added core for the algorithm

--- strategia.py
+++ strategia.py
@@ -100,127 +100,114 @@ def chance(sequence: list())-> int:
         soma += number
     return soma
 
+
+class Sequence:
+    def __init__(self, sequence):
+        self.sequence = sequence
+        self.chance = chance(sequence)
+        self.available = True
+
+class Game:
+
+    def __init__(self):
+        self.sequences = list()
+        #matriz contendo o maior valor possível para cada tipo de score, em ordem crescente de chance
+        #cada sequência será identificada por um id
+        self.highest_scores = list()
+        for i in range(13):
+            self.highest_scores.append(list())
+
+        #sequência os pontos finais
+        self.maximum_sequence = list()
+
+
+    #adiciona um id de uma sequência a um determinado tipo de score
+    def __add_score(self, index, id, chance):
+        i = 0
+        while i < len(self.highest_scores[index]) and chance > self.highest_scores[index][i]:
+            i += 1
+        #temp é usado apenas para adicionar o id na posição que deveria estar
+        self.highest_scores[index] = self.highest_scores[index][:i] + [id] + self.highest_scores[index][i:]
+
+    #para adicionar os scores referentes aos números (1,2,...,6) é necessário
+    #uma política diferente (eles são oordenados pelo score ao invés de chance)
+    def __add_number_scores(self,index, id, score):
+        i = 0
+        while i < len(self.highest_scores[index]) and score < self.highest_scores[index][i]:
+            i += 1
+        
+        self.highest_scores[index] = self.highest_scores[index][:i] + [id] + self.highest_scores[index][i:]
+
+
+
+    def add_sequence(self, sequence: Sequence):
+        id = len(self.sequences)
+        chance = sequence.chance
+        self.sequences.append(sequence)
+        
+        self.__add_number_scores(0, id, ones(sequence.sequence)) #index = 0
+        self.__add_number_scores(1 ,id, twos(sequence.sequence)) #index = 1
+        self.__add_number_scores(2 ,id, threes(sequence.sequence)) #index = 2
+        self.__add_number_scores(3 ,id, fours(sequence.sequence)) #index = 3
+        self.__add_number_scores(4 ,id, fives(sequence.sequence)) #index = 4
+        self.__add_number_scores(5 ,id, six(sequence.sequence)) #index = 5
+
+        self.__add_number_scores(6, id, six(sequence.sequence)) #chance index = 6
+
+        three_like = check_3_like(sequence.sequence) #index = 7
+        four_like = check_4_like(sequence.sequence) #index = 8
+        five_like = check_5_like(sequence.sequence) #index = 9
+        small_straight = check_small_straight(sequence.sequence) #index = 10
+        large_straight = check_large_straight(sequence.sequence) #index = 11
+        full_house = check_full_house(sequence.sequence) #index = 12
+
+        #Esses índices são adotados de tal maneira simplesmente para poder 
+
+        if three_like != 0:
+            self.__add_score(7, id, chance)
+        if  four_like != 0:
+            self.__add_score(8, id, chance)
+        if  five_like != 0:
+            self.__add_score(9, id, chance)
+        if  small_straight != 0:
+            self.__add_score(10, id, chance)
+        if  large_straight != 0:
+            self.__add_score(11, id, chance)
+        if  full_house != 0:
+            self.__add_score(12, id, chance)
+    
+    #IGNORE ESSA FUNÇÃO POR ENQUANTO
+    #é necessário começar atribuindo o ponto para os scores de maior valor primeiro (5_like, full_house, 4_like, ...)
+    #tal qual atualmente, essa função começará pelos 1's
+    def attribute_points(self) -> list():
+        for i in range(len(self.highest_scores)):
+            #iteramos pelas sequências de cada tipo de score até encontrar uma disponível
+            index = 0
+            current_sequence = self.sequences[self.highest_scores[i][index]]
+            while index < len(self.highest_scores[i]):
+                if current_sequence.available == True:
+                    break
+                index += 1
+                current_sequence = self.sequences[self.highest_scores[i][index]]
+            
+
+
+        return
+        
+
 def main():
-    available_scores = [1]*13
-    answers = list()
-    sequences = list()
+    game = Game()
     for i in range(13):
         line = input()
         line_list = line.split()
         for i in range(len(line_list)):
             line_list[i] = int(line_list[i])
-        sequences.append(line_list)
-    resultado = combinatoria(sequences, available_scores, answers)
-    print(resultado)
-    
-def combinatoria(sequences: list(), available_scores: list(), answers: list()) -> list(list()):
-    if not sequences:
-        return answers
+        sequence = Sequence(line_list)
+        game.add_sequence(sequence)
+    resultado = game.attribute_points()
+    soma = sum(resultado)
+    print(resultado, end='')
+    print(soma)
 
-    
-    answer_lists = list(list())
-    
-    for i in range(len(sequences)):
-        current_list = list(sequences)
-        del current_list[i]
-        if available_scores[0] == 1:
-            current_answer = ones(sequences[i])
-            current_types = list(available_scores)
-            current_types[0] = -1
-            answers.append(current_answer)
-            answer_lists.append(combinatoria(current_list, current_types, answers))
-        
-        if available_scores[1] == 1:
-            current_answer = twos(sequences[i])
-            current_types = list(available_scores)
-            current_types[1] = -1
-            answers.append(current_answer)
-            answer_lists.append(combinatoria(current_list, current_types, answers))
-        
-        if available_scores[2] == 1:
-            current_answer = threes(sequences[i])
-            current_types = list(available_scores)
-            current_types[2] = -1
-            answers.append(current_answer)
-            answer_lists.append(combinatoria(current_list, current_types, answers))
-        
-        if available_scores[3] == 1:
-            current_answer = fours(sequences[i])
-            current_types = list(available_scores)
-            current_types[3] = -1
-            answers.append(current_answer)
-            answer_lists.append(combinatoria(current_list, current_types, answers))
-        
-        if available_scores[4] == 1:
-            current_answer = fives(sequences[i])
-            current_types = list(available_scores)
-            current_types[4] = -1
-            answers.append(current_answer)
-            answer_lists.append(combinatoria(current_list, current_types, answers))
-        
-        if available_scores[5] == 1:
-            current_answer = six(sequences[i])
-            current_types = list(available_scores)
-            current_types[5] = -1
-            answers.append(current_answer)
-            answer_lists.append(combinatoria(current_list, current_types, answers))
-        
-        
-        if available_scores[6] == 1:
-            current_answer = chance(sequences[i])
-            current_types = list(available_scores)
-            current_types[6] = -1
-            answers.append(current_answer)
-            answer_lists.append(combinatoria(current_list, current_types, answers))
-            
-        if available_scores[7] == 1:
-            current_answer = check_3_like(sequences[i])
-            current_types = list(available_scores)
-            current_types[7] = -1
-            answers.append(current_answer)
-            answer_lists.append(combinatoria(current_list, current_types, answers))
-            
-        if available_scores[8] == 1:
-            current_answer = check_4_like(sequences[i])
-            current_types = list(available_scores)
-            current_types[8] = -1
-            answers.append(current_answer)
-            answer_lists.append(combinatoria(current_list, current_types, answers))
-            
-        if available_scores[9] == 1:
-            current_answer = check_5_like(sequences[i])
-            current_types = list(available_scores)
-            current_types[9] = -1
-            answers.append(current_answer)
-            answer_lists.append(combinatoria(current_list, current_types, answers))
-            
-        if available_scores[10] == 1:
-            current_answer = check_small_straight(sequences[i])
-            current_types = list(available_scores)
-            current_types[10] = -1
-            answers.append(current_answer)
-            answer_lists.append(combinatoria(current_list, current_types, answers))
-            
-        if available_scores[11] == 1:
-            current_answer = check_large_straight(sequences[i])
-            current_types = list(available_scores)
-            current_types[11] = -1
-            answers.append(current_answer)
-            answer_lists.append(combinatoria(current_list, current_types, answers))
-            
-        if available_scores[12] == 1:
-            current_answer = check_full_house(sequences[i])
-            current_types = list(available_scores)
-            current_types[12] = -1
-            answers.append(current_answer)
-            answer_lists.append(combinatoria(current_list, current_types, answers))
-
-    highest = list()
-    highest_sum = 0
-    for answer_list in answer_lists:
-        soma = sum(answer_list)
-        if soma > highest_sum:
-            highest_sum = soma
-            highest = answer_list
-    return highest
-main()
+if __name__ == '__main__':
+    main()
